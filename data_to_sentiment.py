e0ab0deccf57d27975497a206473ad1a2512fb62
= <=>
2019-11-19 13:25:48 -0500

    rerun sentiment analysis on cleaned data. Only top 2000 prior to manual relabel.

--- data_to_sentiment.py
+++ data_to_sentiment.py
@@ -1,3 +1,7 @@
+"""
+    Run from inside C9 in AWS EC2 instance
+"""
+
 import csv
 import json
 import boto3
@@ -5,34 +9,34 @@ import boto3
 comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
 upper_lim = 2000
 
-with open('data.csv', encoding = "utf8", errors = "ignore") as csv_file_read:
+with open('data_cleaned.csv', encoding = "utf8", errors = "ignore") as csv_file_read:
     with open('sentiments.csv', "a+", encoding = "utf8", errors = "ignore") as csv_file_write:
         csv_writer = csv.writer(csv_file_write, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         csv_reader = csv.reader(csv_file_read, delimiter=',')
         line_count = 0
         for row in csv_reader:
-            if line_count >= upper_lim: 
+            if line_count >= upper_lim:
                 break
             #first row is headers, we set up headers for result file as well
-            if line_count == 0: 
+            if line_count == 0:
                 csv_writer.writerow(row + ['Sentiment', 'Mixed', 'Negative', 'Neutral', 'Positive'])
                 line_count += 1
                 continue
-            
+
             #send request to sentiment analysis with text (row[6])
             temp = json.dumps(comprehend.detect_sentiment(Text=row[6], LanguageCode='en'), sort_keys=True, indent=4)
-            
+
             #convert to a dictionary for easier accessing
             json_dict = json.loads(temp)
             sentiment = json_dict["Sentiment"]
             scores = json_dict["SentimentScore"]
-            
+
             #append the data to the results file
             data_to_add = [sentiment, scores["Mixed"],scores["Negative"],scores["Neutral"],scores["Positive"]]
             csv_writer.writerow(row + data_to_add)
-            print(data_to_add)
+            print(line_count, data_to_add)
             line_count +=1
-            
+
         print("processed lines:", line_count)
 
 #can combine to read input, parse into the text, send text to aws, write response back.
